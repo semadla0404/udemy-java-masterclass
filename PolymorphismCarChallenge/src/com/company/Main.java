2ff7693557db2dd074c0bd40b68433d8c75791b9
semadla0404 <37493233+semadla0404@users.noreply.github.com>
2021-11-06 23:33:57 -0500

    polymorphism challenge

---
+++ PolymorphismCarChallenge/src/com/company/Main.java
@@ -0,0 +1,112 @@
+package com.company;
+
+class Car {
+    private String name;
+    private boolean engine;
+    private int cylinders;
+    private int wheels;
+
+    public Car(String name, int cylinders) {
+        this.name = name;
+        this.engine = true;
+        this.cylinders = cylinders;
+        this.wheels = 4;
+    }
+
+    public String startEngine() {
+        return "Starting the engine...";
+    }
+
+    public String accelerate() {
+        return "Putting the petal to the medal...";
+    }
+
+    public String brake() {
+        return "Slow it on down...";
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Model3 extends Car {
+    public Model3() {
+        super("Model 3", 0);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Starting Engine...  Vrrrrooooooom Model 3";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Starting to Accelerate...  Mr. Musk, ready for take off...";
+    }
+
+    @Override
+    public String brake() {
+        return "Starting to Brake...  Rentry commencing";
+    }
+}
+
+class Jeep extends Car {
+    public Jeep() {
+        super("Jeep Wrangler", 6);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Starting Engine...  Vrrrrooooooom Wrangler";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Starting to Accelerate...  putt putt putt Jeep...";
+    }
+
+}
+
+class Mustang extends Car {
+    public Mustang() {
+        super("Mustang", 8);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Starting Engine...  Vrrrrooooooom Mustang";
+    }
+
+    @Override
+    public String brake() {
+        return "Starting to Brake...  Woah Hoss!";
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Model3 myModel3 = new Model3();
+        System.out.println(myModel3.getName());
+        System.out.println(myModel3.startEngine());
+        System.out.println(myModel3.accelerate());
+        System.out.println(myModel3.brake());
+        System.out.println("=========================");
+
+        Jeep myJeep = new Jeep();
+        System.out.println(myJeep.getName());
+        System.out.println(myJeep.startEngine());
+        System.out.println(myJeep.accelerate());
+        System.out.println(myJeep.brake());
+        System.out.println("=========================");
+
+        Mustang myMustang = new Mustang();
+        System.out.println(myMustang.getName());
+        System.out.println(myMustang.startEngine());
+        System.out.println(myMustang.accelerate());
+        System.out.println(myMustang.brake());
+
+    }
+}
