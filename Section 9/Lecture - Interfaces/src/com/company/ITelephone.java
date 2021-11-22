06a71c11c1dc3b5b3270d1dc0b968e066ef37ae5
semadla0404 <37493233+semadla0404@users.noreply.github.com>
2021-11-22 07:06:24 -0600

    recreated ITelephone interface file

--- Section 9/Lecture - Interfaces/src/com/company/ITelephone.java
+++ Section 9/Lecture - Interfaces/src/com/company/ITelephone.java
@@ -1,31 +1,13 @@
 package com.company;
 
-import java.io.Serializable;
-
 public interface ITelephone {
-
-//    INTERFACE
-//    An interface is basically a template for how you
-//    are designing your class. It ensures your class
-//    follows a well-defined structure.
-//
-//    Consider using interfaces if :
-//            1. You expect that unrelated classes would
-//               implement your interface. For example, many unrelated objects can implement Serializable interface.
-//            2. You want to specify the behavior of a
-//               particular data type, but not concerned about who implements its behavior.
-//            3. You want to take advantage of multiple
-//               inheritance of type.
-//
-//    ARTICLES TO CLARIFY
-//    When do you use either Inheritance, Interfaces or Abstract Classes
-//    * https://www.smashingmagazine.com/2019/11/guide-oop-inheritance-interfaces-abstract-classes/
-//    * https://levelup.gitconnected.com/object-oriented-design-principles-bb6daf98b185
-//    * https://medium.com/javarevisited/oop-good-practices-coding-to-the-interface-baea84fd60d3
-
     void powerOn();
-    void dial(int phoneNumber);
+
+    void dial(int var1);
+
     void answer();
-    boolean callPhone(int phoneNumber);
+
+    boolean callPhone(int var1);
+
     boolean isRinging();
 }
